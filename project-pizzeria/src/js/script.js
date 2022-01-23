dddd0778453422951c78694055cb943ec25feb98
Jakub Churski <churskijakub@gmail.com>
2022-01-23 15:36:55 +0100

    ESlint cahnges

--- project-pizzeria/src/js/script.js
+++ project-pizzeria/src/js/script.js
@@ -91,19 +91,19 @@
       /* START: add event listener to clickable trigger on event click */
       clickableTrigger.addEventListener('click', function(event) {
 
-      /* prevent default action for event */
-      event.preventDefault();
+        /* prevent default action for event */
+        event.preventDefault();
 
-      /* find active product (product that has active class) */
-      const activeProduct = document.querySelector(classNames.menuProduct.wrapperActive);
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(classNames.menuProduct.wrapperActive);
 
-      /* if there is active product and it's not thisProduct.element, remove class active from it */
-      if (activeProduct && activeProduct != thisProduct.element){
-        activeProduct.classList.remove(classNames.menuProduct.wrapperActive);      
-      }
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if (activeProduct && activeProduct != thisProduct.element){
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);      
+        }
 
-      /* toggle active class on thisProduct.element */
-      thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+        /* toggle active class on thisProduct.element */
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
 
       });
 
